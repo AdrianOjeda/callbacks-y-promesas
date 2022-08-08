cba5bd3040bdc6519e25c562f7150d750b4eb599
adrian <adrian.16300022@gmail.com>
2022-08-08 00:24:43 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {buscarHeroe}from './js/callbacks';
+import './styles.css';
+
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+
+buscarHeroe(heroeId1, (err, heroe1)=>{
+   if (err){ return console.error(err);}
+
+   buscarHeroe(heroeId2, (err, heroe2)=>{
+    if (err){ return console.error(err);}
+
+    
+    console.log(`enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+
+
+   });
+   
+});
+
+
+console.log('fin del programa');
